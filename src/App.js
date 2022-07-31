b59f1f17683eb38ce009f078eebb9d2144e295d9
3IMAD69 <dhaimy.imad@gmail.com>
2022-07-31 05:12:36 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ useEffect(()=>{
     <div className='flex items-center justify-center h-screen bg-gray-900'>
       
 <section class="text-gray-400 bg-gray-900 body-font">
-  <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
+  <div class="container mx-auto flex flex-col px-5 py-20 justify-center items-center">
     <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={Logo}/>
     <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">TikTok Voice Generator</h1>
@@ -54,7 +54,7 @@ useEffect(()=>{
         <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
           <label for="hero-field" class="leading-7 text-sm text-gray-400">Your message {count}/300 Characters</label>
           
-          <textarea value={input} maxLength="300" onChange={(e)=>{
+          <textarea value={input} maxLength="300"  onChange={(e)=>{
         setInput(e.target.value)
         setCount(e.target.value.length)
         }} id="message" rows="4" class="block p-2.5 w-full text-sm  rounded-lg border  focus:ring-blue-900  bg-gray-700 border-gray-600 placeholder-gray-400 text-white" placeholder="Your message..."></textarea>
@@ -71,7 +71,13 @@ useEffect(()=>{
 
       <div class="flex text-gray-300">
       {isLoading ? (
-          <h1 className='py-3'>loading...</h1>
+          <div role="status">
+    <svg aria-hidden="true" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
       ):(
         Link ? (
           <div>
@@ -85,7 +91,9 @@ useEffect(()=>{
           </div>
           
         ):(
-          Clicked && <h1>error</h1>
+          Clicked && <div class="p-3 mb-4 text-sm text-red-800 bg-red-200 rounded-lg " role="alert">
+          <span class="font-medium">Error . Invalid Input!</span> 
+        </div>
         )
       )
 
